fa35dcffc471d2089620154641ad1a025a8c003f
Ved Shirur <shirurved18@gmail.com>
2025-02-24 22:56:19 +0530

    Chapter 11 continued

---
+++ Chapter 11 code/04_super.py
@@ -0,0 +1,27 @@
+class Employee:
+    def __init__(self):
+        print("Constructor of Employee")
+    a =1 
+
+class Programmer(Employee):
+    def __init__(self):
+        print("Constructor of Programmer")
+    b = 2 
+
+class Manager(Programmer):
+    def __init__(self):
+        super().__init__()
+    def __init__(self):
+        print("Constructor of Manager")
+    c = 3
+
+o = Employee()
+print(o.a) # Prints the a attribute
+# print(o.b) # Shows an error as there is no b attribute in Employee class
+
+o = Programmer()
+print(o.a, o.b)
+
+
+o = Manager()
+print(o.a, o.b, o.c)
---
+++ Chapter 11 code/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+print(o.a) # Prints the a attribute
+# # print(o.b) # Shows an error as there is no b attribute in Employee class
+
+# o = Programmer()
+# print(o.a, o.b)
